33216fef714e5e0df6f9a15b8694a15cd376ecd7
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-10 14:44:34 +0000

    Added conditions to check if number is smaller or larger

--- console-apps/quess_me/main.js
+++ console-apps/quess_me/main.js
@@ -1,4 +1,24 @@
-const input = require("prompt-sync")()
+const input = require("prompt-sync")();
 
-generatedNumber = Math.floor(Math.random() * 10)
-console.log(generatedNumber)
+generatedNumber = Math.floor(Math.random() * 10);
+
+console.log("Welcome to Guess Me by Jesse Amarquaye");
+console.log("Enjoy this simple game!")
+console.log("I am a number between 0 and 11... guess me");
+
+console.log(generatedNumber);
+
+userNumber = input("Enter number: ");
+
+if (userNumber === generatedNumber){
+    console.log("You had it right");
+} 
+else if (userNumber > generatedNumber){
+    console.log("Incorrect, enter a smaller number (: ");
+}
+else if (userNumber < generatedNumber){
+    console.log("Incorrect, enter a larger number");
+}
+else{
+    console.log("Incorrect number!");
+}
