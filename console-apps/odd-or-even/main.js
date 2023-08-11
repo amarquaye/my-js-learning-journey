0ce5d10294dacc810e547fc0fe50e9548b6048d9
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-11 12:31:46 +0000

    Fixed minor bug

--- console-apps/odd-or-even/main.js
+++ console-apps/odd-or-even/main.js
@@ -1,8 +1,12 @@
 // Simple console-based game to determine whether a number is odd or even.
-// This code was written by Jesse Amarquaye in August, 2023 while learningt the basiscs of javascript. 
+// This code was written by Jesse Amarquaye in August, 2023 while learning the basiscs of javascript. 
 
-const input = require("prompt-sync");
+const input = require("prompt-sync")();
 
+// Start of game 
 console.log("Welcome to Odd or Even");
 console.log("In this game you will enter a number and we'll see if it's odd or even.");
 console.log("Now let's play!\n");
+
+num = input("Enter a number: ");
+console.log(num);
