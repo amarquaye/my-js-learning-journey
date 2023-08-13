a2a832eb93bf89c10e07c4a31f722c46af1068f7
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-13 22:06:12 +0000

    Added feature to check whether the user is a Ghanaian or not.?

--- console-apps/election/main.js
+++ console-apps/election/main.js
@@ -6,11 +6,11 @@ const input = require("prompt-sync")()
 console.log("Welcome to the Election Hub by Jesse Amarquaye (: ");
 let name = input("Enter your name: ");
 let age = input("Enter your age: ");
+let nationality = input("Are you a Ghanaian?...Enter 'y' for yes and 'n' or any character for no: ");
 
-// console.log("Your name is %s and age is %d years.", name, age);
-
-if (age >= 18){
-    console.log("You are at least 18 years years old so you can vote.");
+// Check whether the user is eligible
+if (age >= 18 && nationality === "y"){
+    console.log("You are at least 18 years years old and a Ghanaian so you can vote.");
 }
 else{
     console.log("Sorry %s, you cannot be allowed to vote!", name);
