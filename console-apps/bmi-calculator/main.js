5efbad574a0dcbd05cfb49751b50b70edc3152cb
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-13 22:11:48 +0000

    Updated codebase and fixed typo

--- console-apps/bmi-calculator/main.js
+++ console-apps/bmi-calculator/main.js
@@ -1,3 +1,5 @@
 // Simple program to calculate the body mass index of an individual 
-const input = requre("prompt-sync")()
+const input = require("prompt-sync")()
 
+let mass = input("Enter your mass in kilograms: ");
+console.log("Your mass is %dkg", mass);
