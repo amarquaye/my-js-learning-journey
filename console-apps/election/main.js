8233b10091d0b0fac5d1c2fccb3d6e40ea546145
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-13 21:43:28 +0000

    Initial commit

---
+++ console-apps/election/main.js
@@ -0,0 +1,10 @@
+// Program to check the eligibility of an individual during an election.
+// This program was created by Jesse Amarquaye in August, 2023.
+
+const input = require("prompt-sync")()
+
+console.log("Welcome to the Election Hub by Jesse Amarquaye (: ");
+let name = input("Enter your name: ");
+let age = input("Enter your age: ");
+
+console.log("Your name is %s and age is %d years.", name, age);
