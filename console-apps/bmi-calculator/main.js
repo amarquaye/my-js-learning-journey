ff835b61692c3fdc90f3ba8a6cd73300cfad81e1
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-13 22:13:24 +0000

    Updated

--- console-apps/bmi-calculator/main.js
+++ console-apps/bmi-calculator/main.js
@@ -1,4 +1,6 @@
-// Simple program to calculate the body mass index of an individual 
+// Simple program to calculate the body mass index of an individual.
+// Created by Jesse Amarquaye in August, 2023.
+
 const input = require("prompt-sync")()
 
 let mass = input("Enter your mass in kilograms: ");
