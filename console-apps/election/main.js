9a26c050198c2a9456eb091fdd03e02612a9da09
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-13 21:50:15 +0000

    Added condition to check if user can be allowed to vote

--- console-apps/election/main.js
+++ console-apps/election/main.js
@@ -7,4 +7,11 @@ console.log("Welcome to the Election Hub by Jesse Amarquaye (: ");
 let name = input("Enter your name: ");
 let age = input("Enter your age: ");
 
-console.log("Your name is %s and age is %d years.", name, age);
+// console.log("Your name is %s and age is %d years.", name, age);
+
+if (age >= 18){
+    console.log("You are at least 18 years years old so you can vote.");
+}
+else{
+    console.log("Sorry %s, you cannot be allowed to vote!", name);
+}
