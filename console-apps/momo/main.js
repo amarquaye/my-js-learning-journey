8da28676548696345d22a19dbef3a9512a688553
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-15 15:19:00 +0000

    Initial commit

---
+++ console-apps/momo/main.js
@@ -0,0 +1,6 @@
+// Program to emulate the mobile money interface popularly know as momo in Ghana. 
+// Created by Jesse Amarquaye in August, 2023. 
+
+const input = require("prompt-sync")()
+
+console.log("Menu")
