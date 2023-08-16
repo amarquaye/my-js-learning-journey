caa896db6cc0cb613991bf85ab5848c25133c250
Jesse Amarquaye <96346994+amarquaye@users.noreply.github.com>
2023-08-16 14:31:54 +0000

    Updated up to enter MM PIN

--- console-apps/momo/main.js
+++ console-apps/momo/main.js
@@ -4,6 +4,8 @@
 
 const input = require("prompt-sync")();
 
+const PIN = 1234;
+
 console.log("Menu");
 console.log("1) Transfer Money");
 console.log("2) MoMoPay & Pay Bill");
@@ -28,7 +30,58 @@ switch (num){
         console.log("5) Other Networks");
         console.log("6) Bank Account");
         console.log("0) Back\n");
-        break;
+        // break;
+
+        let transferMoneyOptions = input("Enter response: ");
+        transferMoneyOptions = parseInt(transferMoneyOptions);
+
+        // Options under MoMo User
+        switch (transferMoneyOptions){
+            case 1:
+                let recipientNumber = input("Enter mobile number: ");
+                parseInt(recipientNumber);
+                let recipientNumber2 = input("Confirm Number: ");
+                parseInt(recipientNumber2);
+
+                // Checking if both numbers are the same 
+                if (recipientNumber === recipientNumber2){
+                    let amount = input("Enter Amount: ");
+                    parseFloat(amount);
+                    let ref = input("Enter Reference: ");
+
+                    let fee = 0.00;
+                    let taxAmount = 0.00;
+                    let totalAmount = 0.00;
+
+                    // parseFloat(fee);
+                    // parseFloat(taxAmount);
+                    // parseFloat(totalAmount);
+
+                    if (amount <= 10){
+                        fee = 0.00
+                    } 
+                    else{
+                        fee = 0.01 * amount
+                    }
+                    
+                    taxAmount = 0.01 * amount;
+                    totalAmount = parseFloat(amount) + parseFloat(fee) + parseFloat(taxAmount);
+                    parseFloat(taxAmount);
+            
+                    console.log("\nTransfer to %d for %f with Reference: %s", recipientNumber, amount, ref);
+                    console.log("Fee is GHS %f", fee);
+                    console.log("Tax amount is GHS %f", taxAmount);
+                    console.log("Total Amount is GHS %f", totalAmount);
+                    console.log("\nEnter MM PIN or 2 to cancel.");
+                }
+                else{
+                    console.log("\nNumbers do not match!");
+                }
+                break;
+
+            default:
+                console.error("Please enter a valid number!");
+        }
 
     default:
         console.error("Error, your response %d is not part of the options!", num);
